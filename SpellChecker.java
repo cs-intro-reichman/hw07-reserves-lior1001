1eab7945a223ed598b14f123c2310e518d35b540
Lior Dadonne <Liorgal.dadonne@runi.ac.il>
2024-03-11 09:31:42 +0200

    hw7(1)

--- SpellChecker.java
+++ SpellChecker.java
@@ -16,26 +16,27 @@ public class SpellChecker {
 	}
 
 	public static int levenshtein(String word1, String word2) {
-		int distance = 0;
+		//int distance = 0;
 		String strLow1 = word1.toLowerCase();
 		String strLow2 = word2.toLowerCase();
 		//if |a| = 0
 		if(strLow1.length() == 0) {
-			distance = strLow2.length();
+			return strLow2.length();
 		}
 		//if |b| = 0
 		else if (strLow2.length() == 0) {
-			distance = strLow1.length();
+			return strLow1.length();
 		}
 		//if head(a) = head(b)
 		else if(strLow1.charAt(0) == strLow2.charAt(0)) {
-			distance = levenshtein(tail(strLow1), tail(strLow2));
+			return levenshtein(tail(strLow1), tail(strLow2));
 		}
 		//otherwise
 		else {
-			distance = 1 + (Math.min(levenshtein(tail(strLow1), strLow2), Math.min(levenshtein(strLow1, tail(strLow2)), levenshtein(tail(strLow1), tail(strLow2)))));
+			return 1 + (Math.min(levenshtein(tail(strLow1), strLow2), Math.min(levenshtein(strLow1, tail(strLow2)), levenshtein(tail(strLow1), tail(strLow2)))));
 		}
-		return distance;
+		//System.out.println(strLow1 + " " + strLow2);
+		//return distance;
 	}
 
 	public static String[] readDictionary(String fileName) {
@@ -55,7 +56,7 @@ public class SpellChecker {
 		String closest = word.toLowerCase();
 		//find the word with the smallest distance in the dictionary
 		for(int i=0; i < dictionary.length; i++) {
-			int dis = levenshtein(word, dictionary[i]);
+			int dis = levenshtein(word.toLowerCase(), dictionary[i]);
 			if(dis <= threshold) {
 				closest = dictionary[i];
 			}
